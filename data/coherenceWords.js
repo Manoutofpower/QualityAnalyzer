75d905bd0ad000ac5bb14e397e45e70199e0460d
Manoutofpower <ha19034@bristol.ac.uk>
2024-04-12 02:14:59 +0100

    SSSCRRX-48 Fix 2 word list

--- data/coherenceWords.js
+++ data/coherenceWords.js
@@ -4,7 +4,8 @@ const coherence = [
     "because", "since", "unless", "although", "even though", "while", "when", "whereas", "despite", "in spite of", "regardless",
     "after", "before", "once", "until", "whenever", "during", "as soon as", "by the time",
     "if", "only if", "unless", "until", "provided that", "assuming that", "even if", "in case",
-    "as", "like", "such as", "for example", "for instance", "namely", "to illustrate"
-]
+    "as", "like", "such as", "for example", "for instance", "namely", "to illustrate",
+    "thereby", "besides", "albeit", "notwithstanding", "lest", "shortly", "subsequently", "formerly", "conversely", "comparatively"
+];
 
 export default coherence;
